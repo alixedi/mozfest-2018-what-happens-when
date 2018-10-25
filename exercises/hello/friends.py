4a4c24f70f43a93eb9979f81af24398832c41c20
Ali Zaidi <alixedi@gmail.com>
2018-10-25 21:35:59 +0100

    Adding exercises

---
+++ exercises/hello/friends.py
@@ -0,0 +1,72 @@
+from string import Template
+
+from flask import Flask, request, make_response
+
+
+app = Flask(__name__)
+
+base_template = Template('''
+<!doctype html>
+<head>
+    <style>
+        html, body {
+          height: 100%;
+        }
+        .flex-container {
+          height: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+        form, input, .hello {
+          font-family: monospace;
+        }
+        form, input, .hello {
+          font-size: 40px;
+        }
+        input {
+          border-style: solid;
+          border-width: 2px;
+          padding: 10px;
+          border-radius: 5px;
+        }
+        .name {
+          background-color: yellow;
+        }
+        .message {
+          padding: 50px;
+        }
+    </style>
+</head>
+<body>
+    <div class="flex-container">
+        $content
+    </div>
+</body>
+''')
+
+result_template = Template('''
+    <div class="hello">
+        Hello, <span class="name">$content</span>!
+    </div>
+    <div class="message">
+        Here are some ads for that bag you have been eyeing ;)
+    </div>
+''')
+
+
+@app.route('/')
+def index():
+    name = request.cookies.get('name')
+    if name:
+        return base_template.substitute(
+                content=result_template.substitute(
+                    content=name
+                )
+            )
+    return base_template.substitute(
+            content=result_template.substitute(
+                content="Anonymous"
+            )
+        )
+
---
+++ exercises/hello/hello.py
@@ -0,0 +1,91 @@
+from string import Template
+
+from flask import Flask, request, make_response
+
+
+app = Flask(__name__)
+
+base_template = Template('''
+<!doctype html>
+<head>
+    <style>
+        html, body {
+          height: 100%;
+        }
+        .flex-container {
+          height: 100%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+        form, input, .hello {
+          font-family: monospace;
+        }
+        form, input, .hello {
+          font-size: 40px;
+        }
+        input {
+          border-style: solid;
+          border-width: 2px;
+          padding: 10px;
+          border-radius: 5px;
+        }
+        .name {
+          background-color: yellow;
+        }
+    </style>
+</head>
+<body>
+    <div class="flex-container">
+        $content
+    </div>
+</body>
+''')
+
+result_template = Template('''
+    <div class="hello">
+        Hello, <span class="name">$content</span>!
+    </div>
+''')
+
+form = '''
+        <form action="." method="post">
+            Write your name: <input type="text" name="name">
+        </form>
+'''
+
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    if request.method == 'GET':
+        return base_template.substitute(content=form)
+    else:
+        name = request.form.get('name')
+        return base_template.substitute(
+            content=result_template.substitute(
+                content=name
+            )
+        )
+
+
+@app.route('/cookie', methods=('GET', 'POST'))
+def index_cookie():
+    if request.method == 'GET':
+        if request.cookies.get('name'):
+            return base_template.substitute(
+                content=result_template.substitute(
+                    content=name
+                )
+            )
+        return base_template.substitute(content=form)
+    else:
+        name = request.form.get('name')
+        resp = make_response(
+            base_template.substitute(
+                content=result_template.substitute(
+                    content=name
+                )
+            )
+        )
+        resp.set_cookie('name', name)
+        return resp
